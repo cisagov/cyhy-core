7e79f81120fff606b188fa5f8fbacc066efe3945
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2019-06-24 14:36:33 -0400

    Additional changes based on feedback.

--- var/load_places.sh
+++ var/load_places.sh
@@ -7,44 +7,70 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+function usage() {
+	echo "Usage:  ${0##*/} database_section"
+	exit 0
+}
+
+if [ $# -gt 1 ]
+then
+	usage
+else
+	# Skeleton to allow additional options at a later date if so desired.
+  while getopts ":h-:" opt; do
+		case "${opt}" in
+			"h")
+				usage
+				;;
+			"-")
+				usage
+				;;
+			*)
+				usage
+				;;
+		esac
+	done
+fi
+
+DB_SECTION="${1-}"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DB_SECTION=$1
-DATA_FILE_WEBPAGE_URL="https://www.usgs.gov/core-science-systems/ \
-ngp/board-on-geographic-names/download-gnis-data"
-PLACES_HTML="places_data.html"
+
+# Base URL for the zip files is derived from the listings at
+# https://www.usgs.gov/core-science-systems/ngp/board-on-geographic-names/download-gnis-data
+# for GOVT_UNITS and POP_PLACES
+DATA_BASE_URL="https://geonames.usgs.gov/docs/stategaz/"
+
 TMP_PLACES_DIR="/tmp/places"
 ADDL_PLACES_FILE="../extras/ADDL_CYHY_PLACES.txt"
 
-# Create directory to temporarily store files while working.
+# Create and change to temporary working directory.
 mkdir -p ${TMP_PLACES_DIR}
 cd ${TMP_PLACES_DIR}
-
-curl -o "${PLACES_HTML}" "${DATA_FILE_WEBPAGE_URL}"
 
 # download, unzip and import place files
 # IMPORTANT: GOVT_UNITS must be imported to DB before POP_PLACES and ADDL_CYHY_PLACES
 PLACE_FILES="GOVT_UNITS POP_PLACES"
 for PLACE_TYPE in ${PLACE_FILES}
 do
-	PLACE_URI=$(grep "${PLACE_TYPE}" "${PLACES_HTML}" | cut -d'"' -f2)
+	PLACE_URI="${DATA_BASE_URL}${PLACE_TYPE}.zip"
 	curl -O "${PLACE_URI}"
-	unzip ./*"${PLACE_TYPE}"*.zip -d "${TMP_PLACES_DIR}"
+	unzip "./${PLACE_TYPE}.zip" -d "${TMP_PLACES_DIR}"
 
 	PLACE_FILE=$(ls ${TMP_PLACES_DIR}/*"${PLACE_TYPE}"*.txt)
 	echo "Importing ${PLACE_FILE} to DB..."
 	if [ -z "${DB_SECTION}" ]; then
-		echo "${SCRIPT_DIR}/GNIS_data_import.py" "${PLACE_FILE}"
+		"${SCRIPT_DIR}/GNIS_data_import.py" "${PLACE_FILE}"
 	else
-		echo "${SCRIPT_DIR}/GNIS_data_import.py" -s "${DB_SECTION}" "${PLACE_FILE}"
+		"${SCRIPT_DIR}/GNIS_data_import.py" -s "${DB_SECTION}" "${PLACE_FILE}"
 	fi
 done
 
 # import ADDL_CYHY_PLACES file into the DB
 echo "Importing ${ADDL_PLACES_FILE} to DB..."
 if [ -z "${DB_SECTION}" ]; then
-	echo "${SCRIPT_DIR}/GNIS_data_import.py" "${SCRIPT_DIR}/${ADDL_PLACES_FILE}"
+	"${SCRIPT_DIR}/GNIS_data_import.py" "${SCRIPT_DIR}/${ADDL_PLACES_FILE}"
 else
-	echo "${SCRIPT_DIR}/GNIS_data_import.py" -s "${DB_SECTION}" \
+	"${SCRIPT_DIR}/GNIS_data_import.py" -s "${DB_SECTION}" \
 		"${SCRIPT_DIR}/${ADDL_PLACES_FILE}"
 fi
 
