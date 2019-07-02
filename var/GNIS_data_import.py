8e24b0d929dc17a8c2f10cdc1789c6fb11eaa23c
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2019-07-02 17:07:19 -0400

    Switched to DictReader and field access instead of indices.

--- var/GNIS_data_import.py
+++ var/GNIS_data_import.py
@@ -27,29 +27,37 @@ from cyhy.db import database
 GOVT_UNITS_HEADER = "FEATURE_ID|UNIT_TYPE|COUNTY_NUMERIC|COUNTY_NAME|STATE_NUMERIC|STATE_ALPHA|STATE_NAME|COUNTRY_ALPHA|COUNTRY_NAME|FEATURE_NAME"
 POP_PLACES_HEADER = "FEATURE_ID|FEATURE_NAME|FEATURE_CLASS|STATE_ALPHA|STATE_NUMERIC|COUNTY_NAME|COUNTY_NUMERIC|PRIMARY_LAT_DMS|PRIM_LONG_DMS|PRIM_LAT_DEC|PRIM_LONG_DEC|SOURCE_LAT_DMS|SOURCE_LONG_DMS|SOURCE_LAT_DEC|SOURCE_LONG_DEC|ELEV_IN_M|ELEV_IN_FT|MAP_NAME|DATE_CREATED|DATE_EDITED"
 
-# Exit if file appears to already have been loaded.
+
 def exit_if_imported(fname):
+    """Exit if the file appears to already have been loaded."""
     print "EXITING without importing any documents."
     print "The places collection already has {} loaded.".format(fname)
     sys.exit(0)
 
 
-# Check if the file has already been loaded by seeing if the first and last
-# records are already in the database.
-# NOTE: This method is entirely reliant on Python 2 file reading behavior.
 def is_imported(db, f, type):
+    """Check if the provided file has already been loaded into the database.
+
+    This check is done by seeing if the first and last records are already in
+    the database.
+    NOTE: This method is entirely reliant on Python 2 file reading behavior.
+     """
     # GOVT_UNITS
     if type == 0:
-        name_idx = 9
+        header = GOVT_UNITS_HEADER
     # POP_PLACES
     elif type == 1:
-        name_idx = 1
+        header = POP_PLACES_HEADER
+    else:
+        return False
+
+    name_idx = header.split("|").index("FEATURE_NAME")
 
     marker = f.tell()
     # Get the first record.
     while True:
         first_data_line = f.readline()
-        if first_data_line[0] == "#":  # Skip commented out lines.
+        if first_data_line.lstrip()[0] == "#":  # Skip commented out lines.
             pass
         else:
             break
@@ -62,7 +70,7 @@ def is_imported(db, f, type):
             f.seek(-2, 1)  # Seek to the byte before the one we just read.
         pos = f.tell()
         last_data_line = f.readline()
-        if last_data_line[0] == "#":  # Skip commented out lines.
+        if last_data_line.lstrip()[0] == "#":  # Skip commented out lines.
             # Reset location to one byte before the line just read.
             f.seek(pos)
             f.seek(-1, 1)
@@ -88,24 +96,31 @@ def is_imported(db, f, type):
     return False
 
 
+def skip_comments(lines):
+    """Skip lines that contain a comment."""
+
+    for line in lines:
+        if line.lstrip()[0] != "#":
+            yield line
+
+
 def import_govt_units(db, csv_reader):
     for line in csv_reader:
-        if line[0][0] != "#":  # skip commented-out lines
-            placeDoc = db.PlaceDoc(
-                {
-                    "_id": long(line[0]),
-                    "class": line[1],
-                    "county_fips": line[2],
-                    "county": line[3],
-                    "state_fips": line[4],
-                    "state": line[5],
-                    "state_name": line[6],
-                    "country": line[7],
-                    "country_name": line[8],
-                    "name": line[9],
-                }
-            )
-            placeDoc.save()
+        placeDoc = db.PlaceDoc(
+            {
+                "_id": long(line["FEATURE_ID"]),
+                "class": line["UNIT_TYPE"],
+                "county_fips": line["COUNTY_NUMERIC"],
+                "county": line["COUNTY_NAME"],
+                "state_fips": line["STATE_NUMERIC"],
+                "state": line["STATE_ALPHA"],
+                "state_name": line["STATE_NAME"],
+                "country": line["COUNTRY_ALPHA"],
+                "country_name": line["COUNTRY_NAME"],
+                "name": line["FEATURE_NAME"],
+            }
+        )
+        placeDoc.save()
 
 
 def import_populated_places(db, csv_reader):
@@ -120,34 +135,35 @@ def import_populated_places(db, csv_reader):
         }
 
     for line in csv_reader:
-        if line[0][0] != "#":  # skip commented-out lines
-            placeDoc = db.PlaceDoc(
-                {
-                    "_id": long(line[0]),
-                    "name": line[1],
-                    "class": line[2],
-                    "state": line[3],
-                    "state_fips": line[4],
-                    "county": line[5],
-                    "county_fips": line[6],
-                    "latitude_dms": line[7],
-                    "longitude_dms": line[8],
-                    "latitude_dec": float(line[9]),
-                    "longitude_dec": float(line[10]),
-                }
-            )
-            if line[15]:
-                placeDoc["elevation_meters"] = int(line[15])
-            else:
-                placeDoc["elevation_meters"] = None
-            if line[16]:
-                placeDoc["elevation_feet"] = int(line[16])
-            else:
-                placeDoc["elevation_feet"] = None
-            placeDoc["state_name"] = state_lookup[placeDoc["state"]]["state_name"]
-            placeDoc["country"] = state_lookup[placeDoc["state"]]["country"]
-            placeDoc["country_name"] = state_lookup[placeDoc["state"]]["country_name"]
-            placeDoc.save()
+        placeDoc = db.PlaceDoc(
+            {
+                "_id": long(line["FEATURE_ID"]),
+                "name": line["FEATURE_NAME"],
+                "class": line["FEATURE_CLASS"],
+                "state": line["STATE_ALPHA"],
+                "state_fips": line["STATE_NUMERIC"],
+                "county": line["COUNTY_NAME"],
+                "county_fips": line["COUNTY_NUMERIC"],
+                "latitude_dms": line["PRIMARY_LAT_DMS"],
+                "longitude_dms": line["PRIM_LONG_DMS"],
+                "latitude_dec": float(line["PRIM_LAT_DEC"]),
+                "longitude_dec": float(line["PRIM_LONG_DEC"]),
+            }
+        )
+        if line["ELEV_IN_M"]:
+            placeDoc["elevation_meters"] = int(line["ELEV_IN_M"])
+        else:
+            placeDoc["elevation_meters"] = None
+
+        if line["ELEV_IN_FT"]:
+            placeDoc["elevation_feet"] = int(line["ELEV_IN_FT"])
+        else:
+            placeDoc["elevation_feet"] = None
+
+        placeDoc["state_name"] = state_lookup[placeDoc["state"]]["state_name"]
+        placeDoc["country"] = state_lookup[placeDoc["state"]]["country"]
+        placeDoc["country_name"] = state_lookup[placeDoc["state"]]["country_name"]
+        placeDoc.save()
 
 
 def main():
@@ -160,7 +176,9 @@ def main():
         header_line = (
             place_file.readline().strip().decode("utf-8-sig")
         )  # Files downloaded from geonames.usgs.gov are UTF8-BOM
-        csv_reader = csv.reader(place_file, delimiter="|")
+        csv_reader = csv.DictReader(
+            skip_comments(place_file), delimiter="|", fieldnames=header_line.split("|")
+        )
 
         if header_line == GOVT_UNITS_HEADER:
             if args["--force"] is not True:
