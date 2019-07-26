37fd13402f08e65b1ac166cbed4507ca2897216b
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2019-07-26 12:17:04 -0400

    Update Dockerfile to use LABELs. Add functionality to create an image using a MaxMind license for the GeoIP2 database if available, but fallback to the GeoLite2 database otherwise.

--- var/geoipupdate.sh
+++ var/geoipupdate.sh
@@ -4,10 +4,14 @@
 
 set -o verbose
 
-GEOIP_CITY_URI="http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
+if [ -z $1 ]
+then
+  GEOIP_CITY_URI="https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz"
+else
+  GEOIP_CITY_URI="https://download.maxmind.com/app/geoip_download?edition_id=GeoIP2-City&suffix=tar.gz&license_key=$1"
+fi
 GEOIP_CITY_DIR="/usr/local/share/GeoIP/"
-GEOIP_CITY_FILE="GeoLite2-City.mmdb"
 
 mkdir -p ${GEOIP_CITY_DIR}
 
-curl ${GEOIP_CITY_URI} | gunzip > ${GEOIP_CITY_DIR}/${GEOIP_CITY_FILE}
+curl ${GEOIP_CITY_URI} | tar zxf - --strip-components=1 --directory=${GEOIP_CITY_DIR}
