841702e94b403822238823ecdd96e928b4a9bd2c
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-11-06 15:21:49 -0500

    Update Docker image script to pull license from AWS SSM
    Previously this relied on pulling the MaxMind GeoIP2 license key from SSM
    and then adding it to a file. This file was then pulled in to populate the
    appropriate Docker build-arg. This currently uses awk, which may be fragile
    if AWS changes the response structure. Using jq and switching to
    `--output json` to be more resilient to small API changes.

--- generate_cyhy_docker_image.sh
+++ generate_cyhy_docker_image.sh
@@ -17,8 +17,14 @@ then
 fi
 
 IMAGE_OUTPUT_FILE="cisa_cyhy_core_docker_image_$(date +'%Y%m%d').tgz"
+MAXMIND_LICENSE_KEY=$(aws ssm get-parameter \
+                    --output text \
+                    --name "/cyhy/core/geoip/license_key" \
+                    --with-decryption \
+                    | awk -F"\t" '{print $6}')
+
 
 docker build --tag "$IMAGE_NAME:$IMAGE_TAG" \
              --build-arg maxmind_license_type="full" \
-             --build-arg maxmind_license_key="$(< maxmind_license.txt)" .
+             --build-arg maxmind_license_key="$MAXMIND_LICENSE_KEY" .
 docker save "$IMAGE_NAME:$IMAGE_TAG" | gzip > "$IMAGE_OUTPUT_FILE"
