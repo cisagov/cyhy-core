bedf0567741d067a4bb11c3454c015c7683f00dd
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-11-06 19:19:18 -0500

    Improve functionality of the helper script
    Make the image name and image tag separate command line options to improve
    the flexibility of the tool. Add a dependency check to match up with the Switch
    to using the AWS CLI to get the MaxMind license.

--- generate_cyhy_docker_image.sh
+++ generate_cyhy_docker_image.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+#
+# Generate a cyhy-core Docker image for distribution to the CyHy Team.
 
 set -o nounset
 set -o errexit
@@ -7,14 +9,67 @@ set -o pipefail
 IMAGE_NAME="cisagov/cyhy-core"
 IMAGE_TAG="latest"
 
-if [ $# -eq 1 ]
-then
-  IMAGE_TAG=$1
-elif [ $# -eq 2 ]
-then
-  IMAGE_NAME=$1
-  IMAGE_TAG=$2
-fi
+# Print usage information and exit with the provided exit code.
+function usage {
+  echo "Usage:"
+  echo "  ${0##*/} [options]"
+  echo
+  echo "Options:"
+  echo "  -i, --image-name=NAME  Image name to use [default: $IMAGE_NAME]."
+  echo "  -t, --image-tag=TAG    Image tag to use [default: $IMAGE_TAG]."
+  echo "  -h, --help             Display this message."
+  exit "$1"
+}
+
+# Check for required external programs. If any are missing output a list of all
+# requirements and then exit.
+function check_dependencies {
+  required_tools="aws docker"
+  for tool in $required_tools
+  do
+    if [ -z "$(command -v "$tool")" ]
+    then
+      echo "This script requires the following tools to run:"
+      for item in $required_tools
+      do
+        echo "- $item"
+      done
+      exit 1
+    fi
+  done
+}
+
+while (( "$#" ))
+do
+  case "$1" in
+    -h|--help)
+      usage 0
+      ;;
+    -n|--image-name)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ]
+      then
+        IMAGE_NAME=$2
+        shift 2
+      else
+        usage 1
+      fi
+      ;;
+    -t|--image-tag)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ]
+      then
+        IMAGE_TAG=$2
+        shift 2
+      else
+        usage 1
+      fi
+      ;;
+    -*)
+      usage 1
+      ;;
+  esac
+done
+
+check_dependencies
 
 IMAGE_OUTPUT_FILE="cisa_cyhy_core_docker_image_$(date +'%Y%m%d').tgz"
 MAXMIND_LICENSE_KEY=$(aws ssm get-parameter \
