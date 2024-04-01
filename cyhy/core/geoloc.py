1651b966d039d8e2d56228dd18b4df361f7969de
jeffkause <123761330+jeffkause@users.noreply.github.com>
2024-04-01 14:35:21 -0400

    Update cyhy/core/geoloc.py
    Works for me.
    
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- cyhy/core/geoloc.py
+++ cyhy/core/geoloc.py
@@ -38,7 +38,7 @@ RESTRICTED_COUNTRIES = [
     "Sudan",
     "Syria",
     "Venezuela",
-    "Zimbabwe"
+    "Zimbabwe",
 ]
 # Ensure that the order in GEODB_FILES is used for searching
 for file in GEODB_FILES:
