843497e8436c37f89705c168686bedf7f87bf050
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-07-14 14:46:17 -0400

    Update host state manager tests to account for an additional output value
    It's been many years since the finished_stage output value was added to the transition function in host_state_manager, but we never updated the tests to account for it.

--- cyhy/test/test_host_state_manager.py
+++ cyhy/test/test_host_state_manager.py
@@ -27,25 +27,31 @@ class TestStateManager:
     def test_noops(self, h, state_manager, stage, status):
         h["status"] = status
         h["stage"] = stage
-        was_changed = state_manager.transition(h)
+        was_changed, finished_stage = state_manager.transition(h)
         assert h["stage"] == stage, "stage SHOULD NOT change when status is %s" % status
         assert was_changed == False, '"was_changed" should be False'
+        assert finished_stage == False, '"finished_stage" should be False'
 
     @pytest.mark.parametrize(("status"), [STATUS.WAITING, STATUS.READY, STATUS.RUNNING])
     @pytest.mark.parametrize(("stage"), STAGE)
     def test_ops(self, h, state_manager, stage, status):
         h["status"] = status
         h["stage"] = stage
-        was_changed = state_manager.transition(h)
+        was_changed, finished_stage = state_manager.transition(h)
         assert was_changed == True, '"was_changed" should be True'
+        if status == STATUS.RUNNING:
+            assert finished_stage == True, '"finished_stage" should be True'
+        else:
+            assert finished_stage == False, '"finished_stage" should be False'
 
     @pytest.mark.parametrize(("status"), [STATUS.READY, STATUS.RUNNING])
     @pytest.mark.parametrize(("stage"), STAGE)
     def test_was_failure(self, h, state_manager, stage, status):
         h["status"] = status
         h["stage"] = stage
-        was_changed = state_manager.transition(h, was_failure=True)
+        was_changed, finished_stage = state_manager.transition(h, was_failure=True)
         assert was_changed == True, '"was_changed" should be True'
+        assert finished_stage == False, '"finished_stage" should be False'
         assert (
             h["status"] == STATUS.WAITING
         ), "status should have transitioned from to WAITING but did not."
@@ -56,16 +62,18 @@ class TestStateManager:
     def test_was_failure_no_change(self, h, state_manager, stage, status):
         h["status"] = status
         h["stage"] = stage
-        was_changed = state_manager.transition(h, was_failure=True)
+        was_changed, finished_stage = state_manager.transition(h, was_failure=True)
         assert was_changed == False, '"was_changed" should be False'
+        assert finished_stage == False, '"finished_stage" should be False'
 
     @pytest.mark.parametrize(("status"), [STATUS.WAITING, STATUS.READY])
     @pytest.mark.parametrize(("stage"), STAGE)
     def test_ready_waiting_to_running(self, h, state_manager, stage, status):
         h["stage"] = stage
         h["status"] = status
-        was_changed = state_manager.transition(h)
+        was_changed, finished_stage = state_manager.transition(h)
         assert was_changed == True, '"was_changed" should be True'
+        assert finished_stage == False, '"finished_stage" should be False'
         assert (
             h["status"] == STATUS.RUNNING
         ), "status should have transitioned from READY to RUNNING but did not."
@@ -84,8 +92,9 @@ class TestStateManager:
     def test_running_up_down(self, h, state_manager, i_stage, up, f_stage, f_status):
         h["stage"] = i_stage
         h["status"] = STATUS.RUNNING
-        was_changed = state_manager.transition(h, up=up)
+        was_changed, finished_stage = state_manager.transition(h, up=up)
         assert was_changed == True, '"was_changed" should be True'
+        assert finished_stage == True, '"finished_stage" should be True'
         assert h["stage"] == f_stage, "stage should be %s" % f_stage
         assert h["status"] == f_status, "status should be %s." % f_status
 
@@ -101,8 +110,9 @@ class TestStateManager:
     ):
         h["stage"] = i_stage
         h["status"] = STATUS.RUNNING
-        was_changed = state_manager.transition(h, has_open_ports=has_open_ports)
+        was_changed, finished_stage = state_manager.transition(h, has_open_ports=has_open_ports)
         assert was_changed == True, '"was_changed" should be True'
+        assert finished_stage == True, '"finished_stage" should be True'
         assert h["stage"] == f_stage, "stage should be %s" % f_stage
         assert h["status"] == f_status, "status should be %s." % f_status
 
