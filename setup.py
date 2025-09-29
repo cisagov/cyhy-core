8d81ac3eedd98327118ca4f3794e1f3b72ca675b
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-09-29 10:44:30 -0400

    Bump cyhy-core version from 1.1.4 to 1.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cyhy-core",
-    version="1.1.4",
+    version="1.1.5",
     author="Mark Feldhousen Jr.",
     author_email="mark.feldhousen@cisa.dhs.gov",
     packages=find_packages(),
